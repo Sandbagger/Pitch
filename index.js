a027ef37e494f5a1e3a4e268524be71e2b275e1d
Sandbagger <wneal_19@yahoo.co.uk>
2018-06-02 18:29:34 +0100

    convert typing effect into es6 classes

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 
 /*Welcome splash*/
 
-var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
+let TxtType = class{
+   constructor(el, arrOfStrings) {
+        this.arrOfStrings = arrOfStrings;
         this.el = el;
         this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
+        this.period = 2000;
         this.txt = '';
-        this.tick();
+        this.tick = this.tick.bind(this);
         this.isDeleting = false;
     };
 
-    TxtType.prototype.tick = function(){
- 
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
+    tick(){
+        var i = this.loopNum % this.arrOfStrings.length;
+        var fullTxt = this.arrOfStrings[i];
 
         if (this.isDeleting) {
         this.txt = fullTxt.substring(0, this.txt.length - 1);
@@ -38,43 +38,51 @@ var TxtType = function(el, toRotate, period) {
         delta = 500;
         }
 
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
+        setTimeout(this.tick.bind(this), delta);
+    }
+
+}
 
     window.onload = function() {
         var elements = document.getElementsByClassName('typewrite');
         for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
+            var arrOfStrings = elements[i].getAttribute('data-type');
             var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
+            if (arrOfStrings) {
+              new TxtType(elements[i], JSON.parse(arrOfStrings), period);
             }
         }  
         
     };
 
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var arrOfStrings = elements[i].getAttribute('data-type');
+            if (arrOfStrings) {
+             new TxtType(elements[i], JSON.parse(arrOfStrings));
+            }
+        }  
+        
+    };
+
+
+
+
  document.addEventListener('DOMContentLoaded', setListeners);
 
+let scroll = function (arg){
+    document.querySelector(arg).scrollIntoView({ 
+  behavior: 'smooth', block: "start", inline: "nearest" 
+});
+}
+
+
 function setListeners(){
-  /* document.querySelectorAll(".navbar-brand")[0].
-    addEventListener('click', scroll.bind(this, "#home"));
-   document.querySelectorAll(".nav-link")[0].
-    addEventListener('click', scroll.bind(this, "#bio"));
-   document.querySelectorAll(".nav-link")[1].
-    addEventListener('click', scroll.bind(this, "#projects"));
-   document.querySelectorAll(".nav-link")[2].
-    addEventListener('click', scroll.bind(this, "#contact"));
-*/
+
     $(".arrow-bio").click(scroll.bind(this, "#bio"));
     $(".arrow-portfolio").click(scroll.bind(this, "#projects"));
     $(".arrow-contact").click(scroll.bind(this, "#contact"));
 }
 
-
-function scroll(arg){
-    document.querySelector(arg).scrollIntoView({ 
-  behavior: 'smooth', block: "start", inline: "nearest" 
-});
-}
