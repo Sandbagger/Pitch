a4c8078a6953e50825f7e3a275c9677e7f81ce18
Sandbagger <wneal_19@yahoo.co.uk>
2018-07-07 12:29:00 +0100

    converted typewriter effect to React component

---
+++ Typewriter.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const e = React.createElement;
+
+
+class Typewriter extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			txt: '',
+			fullTxt: '',
+			loopNum: 0,
+			isDeleting: false,
+		}
+	}
+
+	 componentDidMount() {
+	 	this.tick();
+  }
+
+	componentDidUpdate(prevProps, prevState) {
+		 let delta = 200 - Math.random() * 100;
+
+         if (this.state.isDeleting) { delta /= 2; }
+        if (!this.state.isDeleting && this.state.txt === this.state.fullTxt) {
+        delta = this.props.period;
+        } else if (this.state.isDeleting && this.state.txt === '') {
+       delta = 500;
+        }
+
+		 setTimeout(() => this.tick(), delta); 
+	}
+
+	
+
+	tick() {
+		
+		let i = this.state.loopNum % this.props.toRotate.length;
+		
+		let setFullTxt = this.props.toRotate[i];
+
+		let copy = {...this.state};
+
+		   if (this.state.isDeleting) {
+        copy.txt = setFullTxt.substring(0, copy.txt.length - 1);
+        } else {
+        copy.txt = setFullTxt.substring(0, copy.txt.length + 1);
+        }
+
+      
+        if (!this.state.isDeleting && this.state.txt === setFullTxt) {
+      
+        copy.isDeleting = true;
+        } else if (copy.isDeleting && this.state.txt === '') {
+        copy.isDeleting = false;
+        console.log('looping', copy.loopNum, copy.delta)
+        copy.loopNum = copy.loopNum + 1;
+   
+        }
+
+        copy.fullTxt = setFullTxt;
+
+        console.log('copytxt', copy)
+
+        this.setState(() => copy, console.log('delta', this.state.delta))
+	}
+		
+ render() {
+  return e('span',null, this.state.txt)
+ 
+  }
+  
+  
+}
+
+const domContainer = document.querySelector('#typewriter');
+ReactDOM.render(e(Typewriter, {toRotate:["javaScript", 'CSS', 'HTML', 'I am currently teaching myself React <3'], period: 2000}), domContainer);
